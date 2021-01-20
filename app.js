b806182165adeed8c5127fe0ffe21a2c61553625
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2021-01-20 17:14:33 -0300

    indenting the code

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const daysContainer = document.querySelector('#days')
 const hoursContainer = document.querySelector('#hours')
 const minutesContainer = document.querySelector('#minutes')
 const secondsContainer = document.querySelector('#seconds')
+const nextYearContainer = document.querySelector('#year')
+const spinnerLoading = document.querySelector('#loading')
 
 
 
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
+nextYearContainer.textContent = nextYear
+
+
+const getTimeUnity = unit => unit < 10 ? '0' + unit : unit  
+
+const insertCountdownValues = ({ days, hours, minutes, seconds }) => {
+    daysContainer.textContent = getTimeUnity(days)
+    hoursContainer.textContent = getTimeUnity(hours) 
+    minutesContainer.textContent = getTimeUnity(minutes) 
+    secondsContainer.textContent = getTimeUnity(seconds) 
+}
 
 const updateCountdown = () => {
     const currentTime = new Date()
@@ -18,14 +31,10 @@ const updateCountdown = () => {
     const minutes = Math.floor(difference / 1000 / 60) % 60
     const seconds = Math.floor(difference / 1000) % 60
 
-    daysContainer.textContent = days < 10 ? '0' + days : days 
-    hoursContainer.textContent = hours < 10 ? '0' + hours : hours
-    minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
-    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
-    
-
-    console.log({days, hours, minutes, seconds})
-
+    insertCountdownValues({days, hours, minutes, seconds})
 }
 
+setTimeout(()=> {
+    spinnerLoading.remove()
+}, 1000)
 setInterval(updateCountdown, 1000)
