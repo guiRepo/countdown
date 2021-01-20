86c3126fc3a3f5ad7a02c34d54e082601b5bca9c
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2021-01-20 16:42:35 -0300

    get current time

---
+++ app.js
@@ -0,0 +1,22 @@
+const secondsContainer = document.querySelector('#seconds')
+
+
+const nextYear = new Date().getFullYear() + 1
+const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
+
+
+const updateCountdown = () => {
+    const currentTime = new Date()
+    const difference = newYearTime - currentTime
+
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+    const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+    const minutes = Math.floor(difference / 1000 / 60) % 60
+    const seconds = Math.floor(difference / 1000) % 60
+
+
+    console.log({days, hours, minutes, seconds})
+
+}
+
+setInterval(updateCountdown, 1000)
